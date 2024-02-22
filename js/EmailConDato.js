c59f008ecf87c9ee8ba6d69a35c54dc520dfd8f2
Sebita1809 <157644469+Sebita1809@users.noreply.github.com>
2024-02-21 22:43:00 -0300

    Delete js directory

--- js/EmailConDato.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById('datoEmail');
-
-input.addEventListener('input', function() {
-    if (this.value.trim() != '') {
-        this.classList.add('Lleno');
-    } else {
-        this.classList.remove('Lleno');
-    }
-});
